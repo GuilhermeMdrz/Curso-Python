8e764ae6c151da28c13063e50830d2083a03f541
GuilhermeMdrz <gui.medeiros588@gmail.com>
2024-01-23 15:55:53 -0300

    Update ex096.py

--- Mundo 03/ex096.py
+++ Mundo 03/ex096.py
@@ -1,5 +1,6 @@
-def mensagem(msg):
-    print('-' * 30)
-    print(msg)
-    print('-' * 30)
-mensagem('Sistema de alunos')
+def área(a, b):
+    m = a * b
+    print(f'Esse terreno tem {m} metros quadrados.')
+a = float(input('Largura do terreno (m): '))
+b = float(input('Altura do terreno (a): '))
+área(a, b)
