8032265d1c7a0c35f27274fa0494dbeb4f6eb423
GuilhermeMdrz <gui.medeiros588@gmail.com>
2024-01-23 16:13:31 -0300

    Create ex097.py

---
+++ Mundo 03/ex097.py
@@ -0,0 +1,7 @@
+def mensagem(txt):
+    print('~' * len(txt))
+    print(txt)
+    print('~' * len(txt))
+mensagem('Gustavo Guanabara.')
+mensagem('Curso em vídeo.')
+mensagem('O melhor professor que existe.')
