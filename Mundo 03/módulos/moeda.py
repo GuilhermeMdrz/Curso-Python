a3d339553484398ed32e6fd336e2031400ef17a6
GuilhermeMdrz <gui.medeiros588@gmail.com>
2024-01-26 20:24:00 -0300

    Create moeda.py

---
+++ Mundo 03/módulos/moeda.py
@@ -0,0 +1,15 @@
+def aumento(num, aumento):
+    r = num + ((num * aumento) / 100)
+    return r
+
+def desconto(num, desconto):
+    r = num - ((num * desconto) / 100)
+    return r
+
+def dobro(num):
+    r = num * 2
+    return r
+
+def metade(num):
+    r = num / 2
+    return r
