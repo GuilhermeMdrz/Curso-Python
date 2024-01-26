d0379c32dad923433bd9638ffd4a69d253943bed
GuilhermeMdrz <gui.medeiros588@gmail.com>
2024-01-26 20:55:08 -0300

    Update ex108.py

--- Mundo 03/ex108.py
+++ Mundo 03/ex108.py
@@ -1,6 +1,6 @@
 from ex108M import moeda
 num = int(input('Digite um valor: R$'))
-print(f'R${num} com um aumento de 10% fica R${moeda.aumento(num, 10)}')
-print(f'R${num} com um desconto de 10% fica R${moeda.desconto(num, 10)}')
-print(f'O dobro de R${num} é R${moeda.dobro(num)}')
-print(f'A medade de R${num} é de R${moeda.metade(num)}')
+print(f'{moeda.moeda(num)} com um aumento de 10% fica {moeda.moeda(moeda.aumento(num, 10))}')
+print(f'{moeda.moeda(num)} com um desconto de 10% fica {moeda.moeda(moeda.desconto(num, 10))}')
+print(f'O dobro de {moeda.moeda(num)} é {moeda.moeda(moeda.dobro(num))}')
+print(f'A medade de {moeda.moeda(num)} é de {moeda.moeda(moeda.metade(num))}')
