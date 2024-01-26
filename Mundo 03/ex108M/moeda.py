64c51b97b5abb470e68533e0666b04ed5a440cc5
GuilhermeMdrz <gui.medeiros588@gmail.com>
2024-01-26 20:55:17 -0300

    Update moeda.py

--- Mundo 03/ex108M/moeda.py
+++ Mundo 03/ex108M/moeda.py
@@ -1,15 +1,18 @@
-def aumento(num, aumento):
+def aumento(num=0, aumento=0):
     r = num + ((num * aumento) / 100)
     return r
 
-def desconto(num, desconto):
+def desconto(num=0, desconto=0):
     r = num - ((num * desconto) / 100)
     return r
 
-def dobro(num):
+def dobro(num=0):
     r = num * 2
     return r
 
-def metade(num):
+def metade(num=0):
     r = num / 2
     return r
+
+def moeda(num=0, moeda='R$'):
+    return f'{moeda}{num:.2f}'.replace('.', ',')
