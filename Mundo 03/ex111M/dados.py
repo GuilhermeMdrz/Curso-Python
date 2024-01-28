2ad4367dddf93430e9cd63909c612c68caffe42f
GuilhermeMdrz <gui.medeiros588@gmail.com>
2024-01-28 09:04:47 -0300

    Update dados.py

--- Mundo 03/ex111M/dados.py
+++ Mundo 03/ex111M/dados.py
@@ -1,14 +1,2 @@
-def moeda(num=0, moeda='R$'):
-    return f'{moeda}{num:.2f}'.replace('.', ',')
-
-def resumo(num=0, taxaa=10, taxad=10):
-    from ex111M import moeda, dados
-    print('-' * 30)
-    print('RESUMO DO VALOR'.center(30))
-    print('-' * 30)
-    print(f'{"Preço analisado:":20} {dados.moeda(num)}')
-    print(f'{"Dobro do preço:":20} {moeda.dobro(num, True)}')
-    print(f'{"Metade do preço:":20} {moeda.metade(num, True)}')
-    print(f'{"Ápos aumento:":20} {moeda.aumento(num, taxaa, True)}')
-    print(f'{"Ápos desconto:":20} {moeda.desconto(num, taxad, True)}')
-    print('-' * 30)
+def leiadinheiro(msg):
+    if
