624e5db2d374d5a1f5b59c55a97b2a9cc18ac5a4
GuilhermeMdrz <gui.medeiros588@gmail.com>
2024-01-29 20:04:21 -0300

    Create interface.py

---
+++ Mundo 03/ex115M/lib/interface.py
@@ -0,0 +1,27 @@
+def linha(tam = 42):
+    return '-' * tam
+
+def cabeçalho(txt):
+    print(linha())
+    print(txt.center(42))
+    print(linha())
+
+def leiaint(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except:
+            print('\033[31mErro, digite um número inteiro válido!\033[m')
+            continue
+        else:
+            return n
+        
+def menu(lista):
+    cabeçalho('MENU PRINCIPAL')
+    c = 1
+    for item in lista:
+        print(f'\033[33m{c}\033[m - \033[34m{item}\033[m')
+        c +=1
+    print(linha())
+    opc = leiaint('\033[32mSua opção:\033[m ')
+    return opc
